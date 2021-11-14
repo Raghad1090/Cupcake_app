b30dbfb9ac026a6e2967729a64e8a230b89948ab
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-11-14 09:58:02 +0300

    second part

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -19,6 +19,8 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
@@ -26,6 +28,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main){
 
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main){
         val navController  = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 
--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -52,15 +52,8 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding?.startFragment = this
+        binding?.startFragment = this}
 
-        binding?.apply {
-            // Set up the button click listeners
-            orderOneCupcake.setOnClickListener { orderCupcake(1) }
-            orderSixCupcakes.setOnClickListener { orderCupcake(6) }
-            orderTwelveCupcakes.setOnClickListener { orderCupcake(12) }
-        }
-    }
 
     /**
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
@@ -83,5 +76,11 @@ class StartFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_pickupFragment_to_startFragment)
     }
 }
